a557d97f33b73eb80aeba8455f89f30af1a91e40
taquero-programador <jrangel93@protonmail.com>
2022-03-23 11:24:19 -0600

    updatedb.py

--- utils/updatedb.py
+++ utils/updatedb.py
@@ -10,6 +10,13 @@ import csv
 import sqlite3
 from utils import cur, conn
 
-
+# pruebas, no es parte del desarrollo.
 for e in cur.execute('select * from tiempos'):
 	print(e)
+
+r = 'select * from abandono'
+cur.execute(r)
+result = cur.fetchall()
+
+for w in result:
+	print(w)
